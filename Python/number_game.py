0cfd99843f7b1add0ec0c024bea452f9d2c7f7af
Anastasia <anastasia.n.bennett@gmail.com>
2017-05-21 15:11:31 -0700

    Add commented out play_again option where user can decide to play again.

--- Python/number_game.py
+++ Python/number_game.py
@@ -1,28 +1,25 @@
 import random 
 
-#Print "too low" or "too high" messages for bad guesses
-#Let people play again
-
 def game():
-#generate a random number between 1 and 10
+	#generate a random number between 1 and 10
 	secret_num = random.randint(1,10)
 	guesses = []
 
-#Limit the number of guesses
+	#Limit the number of guesses
 	while len(guesses) < 5: 
 		try:
-#get a number guess from the player
+			#get a number guess from the player
 			guess = int(input("Guess a number between 1 and 10: "))
 		except ValueError: 
-#Safely make an int
-#Catch when someone submits a non-integer
+			#Safely make an int
+			#Catch when someone submits a non-integer
 			("{} isn't a number".format(guess))
 		else:
-#compare guess to secret number
+			#compare guess to secret number
 			if guess == secret_num:
 				print("You got it! My number was {}".format(secret_num))
 				break
-#print hit/miss
+			#print hit/miss
 			elif guess < secret_num:
 				print("My number is higher than {}".format(guess))
 			else:
@@ -30,7 +27,10 @@ def game():
 			guesses.append(guess)
 	else:
 		print("You didn't get it. My number was {}".format(secret_num))
-	play_again = input("Do you want to play again? Y/n: ")
-
+	#play_again = input("Do you want to play again? Y/n ")
+	#if play_again.lower() != 'n':
+	#	game()
+	#else:
+	#	print("goodbye")
 
 game()
